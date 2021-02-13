84df17773184681f20a4ce22878e6c1c9daffcbd
Vangelis Giannakosian <vangelis@telesto.gr>
2021-02-13 20:48:41 +0200

    added simple Image comonent

---
+++ components/Image.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Image : React.FC = () => {
+
+    return (
+        <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" alt="Elephant"/>
+    );
+
+}
+
+
+export default Image
